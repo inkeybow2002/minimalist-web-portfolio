b8eb0b6d2900364e454715b10c6c46f30a1dea4b
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 06:10:38 +0100

    feat: initial commit for script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! Thank you for contacting me.');
+    this.reset();
+});
